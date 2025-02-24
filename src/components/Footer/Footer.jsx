46c43afd9ac5e01463f53f2f5c1dcf44903d7565
joseiddd <joseidd@outlook.com>
2025-02-24 10:58:50 -0500

    new update

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./footer.css";
+
+function Footer(){
+
+    return(
+        <footer className="footer">
+            <p className="footer__text"> Developed by Jose Duran <span></span> 2024 </p>
+            
+
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/utils/constants.js
@@ -0,0 +1,118 @@
+export const weatherOptions = [
+  {
+    day: true,
+    condition: "clear",
+    url: new URL("../assets/day/clear.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "cloudy",
+    url: new URL("../assets/day/cloudy.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "rain",
+    url: new URL("../assets/day/rain.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "snow",
+    url: new URL("../assets/day/snow.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "storm",
+    url: new URL("../assets/day/storm.png", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "fog",
+    url: new URL("../assets/day/fog.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "clear",
+    url: new URL("../assets/night/night.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "cloudy",
+    url: new URL("../assets/night/cloudy-dark.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "rain",
+    url: new URL("../assets/night/rain-dark.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "snow",
+    url: new URL("../assets/night/snow-dark.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "storm",
+    url: new URL("../assets/night/storm-dark.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "fog",
+    url: new URL("../assets/night/fog-dark.png", import.meta.url).href,
+  },
+]
+
+export const defaultWeatherOptions = {
+
+  day: {
+    url: new URL("../assets/day/default.png", import.meta.url).href,
+  },
+  night: {
+    url: new URL("../assets/night/default.png", import.meta.url).href,
+  },
+};
+
+export const defaultClothingItems = [
+    {
+      _id: 0,
+      name: "Cap",
+      weather: "hot",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Cap.png?etag=f3dad389b22909cafa73cff9f9a3d591",
+    },
+    {
+      _id: 1,
+      name: "Hoodie",
+      weather: "warm",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Hoodie.png?etag=5f52451d0958ccb1016c78a45603a4e8",
+    },
+    {
+      _id: 2,
+      name: "Jacket",
+      weather: "cold",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Jacket.png?etag=f4bb188deaa25ac84ce2338be2d404ad",
+    },
+    {
+      _id: 3,
+      name: "Sneakers",
+      weather: "cold",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Sneakers.png?etag=3efeec41c1c78b8afe26859ca7fa7b6f",
+    },
+    {
+      _id: 4,
+      name: "T-Shirt",
+      weather: "hot",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/T-Shirt.png?etag=44ed1963c44ab19cd2f5011522c5fc09",
+    },
+    {
+      _id: 5,
+      name: "Coat",
+      weather: "cold",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Coat.png?etag=298717ed89d5e40b1954a1831ae0bdd4",
+    },
+  ];
+
+export const coordinates = {
+  latitude:40.603917,
+  longitude:-74.058624,
+};
+
+export const APIkey = "e2438819d17df5e4e419b24b66bc1611";
