fff0917add24dc0f99a73991d8fb07e6e07def5c
joseiddd <joseidd@outlook.com>
2025-02-25 12:00:46 -0500

    new update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,13 @@
 import "./footer.css";
 
-function Footer(){
-
-    return(
-        <footer className="footer">
-            <p className="footer__text"> Developed by Jose Duran <span></span> 2024 </p>
-            
-
-        </footer>
-    )
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__text">
+        Developed by Jose Duran <span></span>2024
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,24 +1,26 @@
 import "./ItemModal.css";
 import closeiconwhite from "../../assets/closeiconwhite.png";
 
-function ItemModal ({ activeModal, onClose, card}) {
-return (
+function ItemModal({ activeModal, onClose, card }) {
+  return (
     <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
-        <div className="modal__content modal__content_type_image">
+      <div className="modal__content modal__content_type_image">
         <button onClick={onClose} type="button" className="modal__close">
-            <img src={closeiconwhite} alt="closeiconwhite" className="modal__close-btn" />
+          <img
+            src={closeiconwhite}
+            alt="closeiconwhite"
+            className="modal__close-btn"
+          />
         </button>
-        
-        <img src={card.link} alt="" className="modal__image" />  
+
+        <img src={card.link} alt={card.name} className="modal__image" />
         <div className="modal__footer">
-            <h2 className="modal__caption">{card.name}</h2>
-            <p className="modal__weather">Weather: {card.weather}</p>
-            
-            
-            </div> 
+          <h2 className="modal__caption">{card.name}</h2>
+          <p className="modal__weather">Weather: {card.weather}</p>
         </div>
+      </div>
     </div>
-)
+  );
 }
 
 export default ItemModal;
